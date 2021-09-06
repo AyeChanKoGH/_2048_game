c71639acfdaff523b37766fcbfc84f507d3a461a
FlutLab.io <>
2021-09-06 12:57:26 +0000

    Autosave

--- lib/widget/color.dart
+++ lib/widget/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 Map<String, Color?> colormatch = {
-  '0': Colors.white38,
+  '0': Colors.white54,
   '2': Colors.blue,
   '4': Colors.tealAccent,
   '8': Colors.indigo,
