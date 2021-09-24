d147176e1d414b782ec17bc097350ddcc58329ad
FlutLab.io <>
2021-09-24 08:13:24 +0000

    Autosave

--- lib/backend/array.dart
+++ lib/backend/array.dart
@@ -139,13 +139,11 @@ class ArrayTile {
 
   bool blocked() {
     List? vArray = array?.map((row) => row.map((obj) => obj.value).toList()).toList();
-    print(vArray);
-    bool canSwipUpDown = vArray?.map((n) => vArray.map((row) => row[vArray.indexOf(n)])).any((row) => checkAdjacentDuplicate(row)) ?? false;
     bool havespace = vArray?.expand((row) => row).contains(0) ?? false;
     if (havespace) return false;
     bool canSwipLeftRight = vArray?.any((row) => checkAdjacentDuplicate(row)) ?? false;
     if (canSwipLeftRight) return false;
-
+    bool canSwipUpDown = vArray?.map((n) => vArray.map((row) => row[vArray.indexOf(n)])).any((row) => checkAdjacentDuplicate(row)) ?? false;
     if (canSwipUpDown) return false;
     return true;
   }
