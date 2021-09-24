d0690e4df6ada2ff68399128e02936a37df0cdcf
FlutLab.io <>
2021-09-24 08:19:35 +0000

    Autosave

--- lib/widget/valueContainer.dart
+++ lib/widget/valueContainer.dart
@@ -6,6 +6,6 @@ class ValueContainer extends StatelessWidget {
   ValueContainer(this.value);
   @override
   Widget build(BuildContext context) {
-    return Card(color: colormatch['${value}'], child: Center(child: Text('${value}', style: TextStyle(fontSize: 20))));
+    return Card(color: colormatch['${value}'], child: Center(child: Text('${value}', style: TextStyle(fontSize: 25))));
   }
 }
